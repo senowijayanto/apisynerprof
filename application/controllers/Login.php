bbde224f15fdfa34b95481c358e76c2f53bede13
Seno Wijayanto <senowijayanto@gmail.com>
2017-06-18 10:26:02 +0700

    First init

---
+++ application/controllers/Login.php
@@ -0,0 +1,50 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+
+class Login extends REST_Controller {
+
+  function __construct() {
+    parent::__construct();
+  }
+
+  public function index_get() {
+    $member_id  = $this->get('member_id');
+    $password   = $this->get('password');
+
+    if (empty($member_id) && empty($password)) {
+      $this->set_response([
+        'status'  => FALSE,
+        'message' => 'Member ID dan Password tidak boleh kosong.'
+      ], REST_Controller::HTTP_BAD_REQUEST);
+    }
+
+    $this->db->where('member_id', $member_id);
+    $this->db->where('password', sha1($password));
+    $user = $this->db->get('users')->result();
+
+    if (!$user) {
+      $this->response([
+        'status'  => FALSE,
+        'message' => 'Member ID dan Password Salah.'
+      ], REST_Controller::HTTP_BAD_REQUEST);
+    }
+
+    $data = [];
+    foreach ($user as $value) {
+      $data['id']         = $value->id;
+      $data['member_id']  = $value->member_id;
+      $data['name']       = $value->name;
+      $data['email']      = $value->email;
+    }
+
+    $this->response([
+      'data'    => $data,
+      'status'  => TRUE,
+      'message' => 'Login Sukses'
+    ], REST_Controller::HTTP_OK);
+
+    exit();
+  }
+}
---
+++ application/controllers/Users.php
@@ -0,0 +1,136 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+
+class Users extends REST_Controller {
+
+  function __construct() {
+    parent::__construct();
+  }
+
+  public function index_get() {
+    $users = $this->db->get('users')->result();
+
+    $id = $this->get('id');
+
+    // If the id parameter doesn't exist return all the users
+
+    if ($id === NULL)
+    {
+        // Check if the users data store contains users (in case the database result returns NULL)
+        if ($users)
+        {
+            // Set the response and exit
+            $this->response($users, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+        }
+        else
+        {
+            // Set the response and exit
+            $this->response([
+                'status' => FALSE,
+                'message' => 'No users were found'
+            ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+        }
+    }
+
+    // Find and return a single record for a particular user.
+
+    $id = (int) $id;
+
+    // Validate the id.
+    if ($id <= 0)
+    {
+        // Invalid id, set the response and exit.
+        $this->response(NULL, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+    }
+
+    $user = NULL;
+
+    if (!empty($users))
+    {
+      $this->db->where('id', $id);
+      $user = $this->db->get('users')->result();
+    }
+
+    if (!empty($user))
+    {
+        $this->set_response($user, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+    }
+    else
+    {
+        $this->set_response([
+            'status' => FALSE,
+            'message' => 'User could not be found'
+        ], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+    }
+  }
+
+  public function index_post() {
+    $data = array(
+      'member_id' => $this->post('member_id'),
+      'name'      => $this->post('name'),
+      'email'     => $this->post('email'),
+      'password'  => sha1($this->post('password'))
+    );
+
+    // Check existing member ID
+    $this->db->where('member_id', trim($this->post('member_id')));
+    $check_ID = $this->db->get('users')->result();
+
+    if ($check_ID) {
+      $this->set_response([
+        'status'  => FALSE,
+        'message' => 'Member ID '.$this->post('member_id').' sudah ada.'
+      ], REST_Controller::HTTP_BAD_REQUEST);
+    } else {
+      $insert = $this->db->insert('users', $data);
+      if ($insert) {
+        $this->response([
+          'data'    => $data,
+          'status'  => TRUE,
+          'message' => 'Insert Success'
+        ], REST_Controller::HTTP_OK);
+      } else {
+        $this->response(array('status' => 'fail', 502));
+      }
+    }
+  }
+
+  public function index_put() {
+    $member_id = $this->put('member_id');
+    $data = array(
+      // 'name'      => $this->put('name'),
+      // 'email'     => $this->put('email'),
+      // 'password'  => sha1($this->put('password')),
+      'status'    => $this->put('status')
+    );
+
+    // Check existing email
+    $this->db->where('member_id', trim($member_id));
+    $check_ID = $this->db->get('users')->result();
+
+    if (!$check_ID) {
+      $this->set_response([
+        'status'  => FALSE,
+        'message' => 'Member ID is not exist'
+      ], REST_Controller::HTTP_BAD_REQUEST);
+    } else {
+      $this->db->where('member_id', trim($member_id));
+      $update = $this->db->update('users', $data);
+      if ($update) {
+        $this->response([
+          'status'  => TRUE,
+          'message' => 'Update Success'
+        ], REST_Controller::HTTP_OK);
+      } else {
+        $this->response(array('status' => 'fail', 502));
+      }
+    }
+  }
+
+  public function index_delete() {
+    $member_id = $this->delete('member_id');
+    $this->response(array('status' => $member_id, 201));
+  }
+}
